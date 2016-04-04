6860c5404f0f93cf6884c24981ceb2b314956ca1
Maxim Polishchuck <mpolishchuck@gmail.com>
2016-04-05 02:38:19 +0300

    zoom support and aspect ratio saving

--- main.cpp
+++ main.cpp
@@ -6,16 +6,55 @@
 #define Y1   -200
 #define Y2   200
 
+#define ZOOM_STEP  3
+
+#define SET_GLOBAL_STATE_SIZE(__w, __h) \
+    gState.iWidth = __w; \
+    gState.iHeight = __h;
+
 using namespace std;
 
-void Reshape(int width, int height)
+typedef struct {
+    int iZoom = 0;
+    int iWidth;
+    int iHeight;
+} globalState;
+
+globalState gState;
+
+GLvoid hSetupOrtho2D()
 {
-    glViewport(0, 0, width, height);
     glLoadIdentity();
-    gluOrtho2D(X1, X2, Y1, Y2);
+
+    int iXCorrection = 0;
+    int iYCorrection = 0;
+
+    double dblDeltaAspectRatio;
+
+    if (gState.iWidth > gState.iHeight) {
+        dblDeltaAspectRatio = (double)gState.iWidth / (double)gState.iHeight;
+        iXCorrection = ((double)(Y2 - Y1) * dblDeltaAspectRatio - (Y2 - Y1)) / 2;
+    } else if (gState.iWidth < gState.iHeight) {
+        dblDeltaAspectRatio = (double)gState.iHeight / (double)gState.iWidth;
+        iYCorrection = ((double)(X2 - X1) * dblDeltaAspectRatio - (X2 - X1)) / 2;
+    }
+
+    gluOrtho2D(
+                X1 - gState.iZoom - iXCorrection,
+                X2 + gState.iZoom + iXCorrection,
+                Y1 - gState.iZoom - iYCorrection,
+                Y2 + gState.iZoom + iYCorrection
+    );
 }
 
-void Draw()
+GLvoid handleReshape(int width, int height)
+{
+    glViewport(0, 0, width, height);
+    SET_GLOBAL_STATE_SIZE(width, height);
+    hSetupOrtho2D();
+}
+
+GLvoid handleDraw(GLvoid)
 {
     glClear(GL_COLOR_BUFFER_BIT);
 
@@ -42,19 +81,44 @@ void Draw()
     glEnd();
 
     glFlush();
+    glutSwapBuffers();
+}
+
+GLvoid handleMouse(int button, int state, int x, int y)
+{
+    if (state == 0) {
+        switch (button) {
+        case 3:
+            gState.iZoom -= ZOOM_STEP;
+            break;
+
+        case 4:
+            gState.iZoom += ZOOM_STEP;
+            break;
+        }
+    }
+
+    hSetupOrtho2D();
+}
+
+GLvoid handleIdle(GLvoid) {
+    glutPostRedisplay();
 }
 
 int main(int argc, char *argv[])
 {
     glutInit(&argc, argv);
     glutInitWindowSize(300, 300);
+    SET_GLOBAL_STATE_SIZE(300, 300);
     glutInitWindowPosition(100, 100);
 
-    glutInitDisplayMode(GLUT_RGB);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
     glutCreateWindow("FreeGLUT example");
 
-    glutReshapeFunc(Reshape);
-    glutDisplayFunc(Draw);
+    glutReshapeFunc(handleReshape);
+    glutDisplayFunc(handleDraw);
+    glutMouseFunc(handleMouse);
+    glutIdleFunc(handleIdle);
 
     glClearColor(1, 1, 1, 1);
 
